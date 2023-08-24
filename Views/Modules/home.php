fd19f8cbc48d3f8229e3794bba79c84040339a36
DenisManuel <142513646+DenisManuel@users.noreply.github.com>
2023-08-24 16:06:28 -0600

    comm

--- Views/Modules/home.php
+++ Views/Modules/home.php
@@ -260,7 +260,12 @@ $rango2=intval(strtotime($fechafin));
                                     </div>
                                              </div>
                                              <div class="price_table_bottom">
-                                                <div class="center"><a class="main_bt" href="travelinfo">View Details</a></div>
+                                                <form action="travelinfo" class="center" method="POST">
+                                                 
+                                                   <input value="<?= $fechainicio ?>" type="hidden" name="finicio" id="finicio">
+                                                   <input value="<?= $fechafin ?>" type="hidden" name="ffin" id="ffin">
+                                                   <input type="submit" class="main_bt" value="View Details"> 
+                                                </form>
                                              </div>
                                           </div>
                                        </div>
@@ -305,7 +310,12 @@ $rango2=intval(strtotime($fechafin));
                                     </div>
                                              </div>
                                              <div class="price_table_bottom">
-                                                <div class="center"><a class="main_bt" href="amenitiesinfo">View Details</a></div>
+                                                <form action="tourinfo" class="center" method="POST">
+                                                 
+                                                   <input value="<?= $fechainicio ?>" type="hidden" name="finicio" id="finicio">
+                                                   <input value="<?= $fechafin ?>" type="hidden" name="ffin" id="ffin">
+                                                   <input type="submit" class="main_bt" value="View Details"> 
+                                                </form>
                                              </div>
                                           </div>
                                        </div>
