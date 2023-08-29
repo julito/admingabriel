fa45a0352896d6bfcfd527189c7e78dc8d674fb3
julito <jcponce87@gmail.com>
2023-08-29 16:54:27 -0600

    funciones de eliminar

--- Views/assets/js/funciones.js
+++ Views/assets/js/funciones.js
@@ -46,10 +46,61 @@ $(document).ready(function(){
 
 
 
+    $(document).on("click",".btnEliminaramenities",function(){
+        var idRes = $(this).attr("idRes");
+       
+
+
+        Swal.fire({
+            title: 'Do you want to delete item?',
+            
+            showCancelButton: true,
+            confirmButtonText: 'Save',
+            
+          }).then((result) => {
+            
+            if (result.isConfirmed) {
+                var url=$("#rutaurl").val()+'Models/reservasModel.php';
+                var token=$("#tokenamenities").val();
+
+                $.ajax({
+                    type: "POST",
+                    url: url,
+                    data: {'accion':'delete','id':idRes,'api':3,'token':token},
+                    beforeSend: function() {
+                
+                    },
+                    success: function(respuesta) {
+                             console.log(respuesta)                ;
+                        if(respuesta==true)
+                        {
+                            Swal.fire('Deleted!', '', 'success')
+                            location.reload(false);
+                        }
+                        else
+                        Swal.fire('Error!', '', 'error')
+                    }
+                  });
+
+
+            } 
+          })
+       
+    })
+    
+
+
+
+
+
+
+
+
+
     $("#makereservasconcierge").on('click',function()
     {  
-    var fecha1=$("#date1").val();
-    var fecha2=$("#date2").val();
+    var fecha1=$("#rdate").val();
+    var fecha2=$("#rdate2").val();
     var hotel=$("#hotelorigen").val();
     var url=`http://conciergehotline.net/api/reservaciones?select=*&between1=${fecha1}&between2=${fecha2}&filterTo=reservaciones_cupon&inTo=${hotel}&Columna=hecho`;
   
@@ -64,17 +115,18 @@ $(document).ready(function(){
         success: function(respuesta) {
           datos = respuesta.results;
           
-          datos.forEach(function(item) {
+          datos.forEach(async function(item) {
             if (item.estado == 0) {
             
               var token=$("#tokenconcierge").val();
 
 
              var puturl=`http://conciergehotline.net/api/reservaciones?id=${item.reservaciones_id}&nameId=reservaciones_id&token=${token}` ;
-
-              $.ajax({
+            try{
+             const response= await $.ajax({
                 type: "PUT",
                 url: puturl,
+                async:true,
                 data: {'estado':1},
                 beforeSend: function() {
             
@@ -85,7 +137,9 @@ $(document).ready(function(){
             
                 }
               });
-
+            } catch (putError) {
+                console.error("Error in PUT request:", putError);
+            }
 
             }
           });
