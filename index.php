50a995b51870922cbad5d9bc5fcf75c817ef101c
julito <jcponce87@gmail.com>
2023-08-31 11:36:04 -0600

    cambios de admin

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+
+
 session_start();
 
 $_SESSION["HOTEL"]="GABRIEL";
@@ -8,7 +11,7 @@ define('VISTAS',ROOT.'Views'.DS.'Modules'.DS);
 define('API_CONCIERGE','http://www.conciergehotline.net/api/');
 define('API_TRAVEL','http://travelvipmiami.com/api/');
 define('API_AMENITIES','http://hotelroomdecoration.com/api/');
-define('RUTAURL','http://192.168.1.103/admingabriel/');
+define('RUTAURL','http://localhost/admingabriel/');
 
 define('RUTAASSETS','http://localhost/admingabriel/views/assets/');
 
@@ -23,6 +26,10 @@ require_once "Models/rutas.php";
 require_once "Models/reservasModel.php";
 require_once "Models/enlacesModel.php";
 require_once "Models/loginModel.php";
-
-$plantilla = new PlantillaC();
-$plantilla -> cargarPlantilla();
+try{
+  $plantilla = new PlantillaC();
+$plantilla -> cargarPlantilla();  
+}
+catch(Exception $e){
+header("Location:home");
+}
