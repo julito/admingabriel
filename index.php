eceb1592bce5e7f2ac0a7ee91b466aa436cf79be
DenisManuel <142513646+DenisManuel@users.noreply.github.com>
2023-08-31 10:39:46 -0600

    comm

--- index.php
+++ index.php
@@ -8,9 +8,9 @@ define('VISTAS',ROOT.'Views'.DS.'Modules'.DS);
 define('API_CONCIERGE','http://www.conciergehotline.net/api/');
 define('API_TRAVEL','http://travelvipmiami.com/api/');
 define('API_AMENITIES','http://hotelroomdecoration.com/api/');
-define('RUTAURL','http://192.168.1.103/admingabriel/');
+define('RUTAURL','http://192.168.1.105/admingabriel/');
 
-define('RUTAASSETS','http://localhost/admingabriel/views/assets/');
+define('RUTAASSETS','http://192.168.1.105/admingabriel/views/assets/');
 
 require_once "Controllers/plantillaController.php";
 require_once "Controllers/reservasController.php";
